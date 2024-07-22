dbee3d82438a46fd3ca20ec2c58b53b7652a0eec
Juan Montoya <jpmontoya182@gmail.com>
2024-07-22 16:37:29 -0500

    adding files

---
+++ game.js
@@ -0,0 +1,32 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 256,
+  height: 244,
+  backgroundColor: "#049cd8",
+  parent: "game",
+  scene: {
+    preload, // precargar recursos
+    create, // inicial el juego
+    update, // en cada frame
+  },
+}
+
+new Phaser.Game(config)
+
+function preload() {
+  this.load.image("cloud1", "assets/scenery/overworld/cloud1.png")
+  this.load.image("floorbricks", "assets/scenery/overworld/floorbricks.png")
+  this.load.spritesheet("mario", "assets/entities/mario.png", {
+    frameWidth: 18,
+    frameHeight: 16,
+  })
+}
+function create() {
+  // image(x, y,asset-id)
+  this.add.image(0, 0, "cloud1").setOrigin(0, 0).setScale(0.15)
+  this.add
+    .tileSprite(0, config.height, config.width, 32, "floorbricks")
+    .setOrigin(0, 1)
+  this.add.sprite(48, 211, "mario").setOrigin(0, 1)
+}
+function update() {}
